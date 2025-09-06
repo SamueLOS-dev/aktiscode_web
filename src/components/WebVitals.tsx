963cc060d93efe00671bbee2d307e0abc4a58017
Samuel Hlavačka <78475575+SamueLOS-dev@users.noreply.github.com>
2025-09-06 13:12:11 +0200

    Fix web-vitals imports using individual module paths

--- src/components/WebVitals.tsx
+++ src/components/WebVitals.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
-import { getCLS, getFID, getFCP, getLCP, getTTFB, Metric } from 'web-vitals';
+import { getCLS } from 'web-vitals/dist/getCLS';
+import { getFID } from 'web-vitals/dist/getFID';
+import { getFCP } from 'web-vitals/dist/getFCP';
+import { getLCP } from 'web-vitals/dist/getLCP';
+import { getTTFB } from 'web-vitals/dist/getTTFB';
+import type { Metric } from 'web-vitals';
 
 const WebVitals = () => {
   useEffect(() => {
